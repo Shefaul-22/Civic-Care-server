48064031625b337c5d38ed414434028a02093188
Shefaul-22 <cse1805022brur@gmail.com>
2026-01-20 11:56:02 +0600

    Mongodb database connection setup done

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+require('dotenv').config();
 
-app.get('/', (req,res) => {
+
+app.get('/', (req, res) => {
     res.send("CivicCare is running")
 })
 
-app.listen(port, ()=> {
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@crud-server-practices.rbtbow5.mongodb.net/?appName=crud-server-practices`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
     console.log(`CivicCare server is running on port ${port}`);
 })
