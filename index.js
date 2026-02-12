baddfb63c869d7f91249b299915313283e4110eb
Shefaul-22 <cse1805022brur@gmail.com>
2026-02-12 21:29:43 +0600

    Issues get related api updated

--- index.js
+++ index.js
@@ -77,6 +77,9 @@ const client = new MongoClient(uri, {
 app.use(cors())
 app.use(express.json())
 
+
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
